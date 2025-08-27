fec7ce66d5d39db4921bc59dadd251f3dacb6e2b
NOELINEDONGO <101535827+NOELINEDONGO@users.noreply.github.com>
2025-08-27 12:27:32 +0000

    second commit

--- config.py
+++ config.py
@@ -4,8 +4,10 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    jwt_secret: str = Field(env="JWT_SECRET_KEY")
-    jwt_algorithm: str = Field(env="JWT_ALGORITHM")
+    jwt_secret: str  #= Field(env="JWT_SECRET_KEY")
+    jwt_algorithm: str  #= Field(env="JWT_ALGORITHM")
     class Config:
         env_file = ".env"
+
+
 settings = Settings()
